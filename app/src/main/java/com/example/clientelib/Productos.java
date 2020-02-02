bde15581fb0dadf1d5db4b2b4cd583ff9ede9d32
MayraLlanos <mllanosq@est.ups.edu.ec>
2020-02-02 01:59:28 -0500

    Comit float funcionando

--- app/src/main/java/com/example/clientelib/Productos.java
+++ app/src/main/java/com/example/clientelib/Productos.java
@@ -3,8 +3,12 @@ package com.example.clientelib;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -13,20 +17,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class Productos extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
-    private Button carrito;
+    private ImageButton carrito;
     private ImageButton like;
+    private ImageButton compartir;
+    private TextView cantidad;
     private MainActivity principal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos);
+        carrito=(ImageButton)findViewById(R.id.añadirbtn);
+        like=(ImageButton)findViewById(R.id.likebtn);
+        compartir=(ImageButton)findViewById(R.id.compartirbtn);
+        car = (FloatingActionButton)findViewById(R.id.floating);
+        car.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), precompra.class);
+                startActivityForResult(i,0);
+            }
+        });
+        cantidad=(TextView)findViewById(R.id.cantidadtxt);
 
         ArrayList<Items> listaItems = new ArrayList<>();
         //listaItems.add(new Items(R.drawable.ic_android1,"Linea 1","Linea 2"));
@@ -40,8 +62,8 @@ public class Productos extends AppCompatActivity {
 
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
-        carrito=(Button)findViewById(R.id.añadirtxt);
-        like=(ImageButton)findViewById(R.id.likebtn);
+
+
         String usuario= getFromSharedPreferences("usuario");
         System.out.println("usuario de shared preferences"+usuario);
         Toast.makeText(getApplicationContext(),"Bienvenid@"+usuario,Toast.LENGTH_LONG).show();
